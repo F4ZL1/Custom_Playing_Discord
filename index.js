61c32e8a0c5cbe8c4cbca11b1d6657dc680b04cb
Fazli <72749051+F4ZL1@users.noreply.github.com>
2021-05-14 22:59:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+details : "Bergabunglah Di Komunitas Kami",
+assets : {
+large_image : "image",
+large_text : "Your Status" // THIS WILL SHOW AS "Playing <Status>" from the outisde
+},
+buttons : [{label : "#INDOGHOST ROLEPLAY" , url : "https://discord.gg/CasW8qyEeu"},{label : "CAPITAL ROLEPLAY",url : "https://discord.gg/TXYarkzdB4"}]
+}
+})
+})
+client.login({ clientId : "842783861569093654" }).catch(console.error);
